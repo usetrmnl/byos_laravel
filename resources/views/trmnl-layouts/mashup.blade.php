97e6beaee48c5d34073e1516ea681ec53cf42b8d
Benjamin Nussbaum <bnussbau@users.noreply.github.com>
2025-09-18 11:44:57 +0200

    feat: prepare mashup

--- resources/views/trmnl-layouts/mashup.blade.php
+++ resources/views/trmnl-layouts/mashup.blade.php
@@ -1,8 +1,25 @@
-@props(['mashupLayout' => '1Tx1B'])
+@props([
+    'mashupLayout' => '1Tx1B',
+    'noBleed' => false,
+    'darkMode' => false,
+    'deviceVariant' => 'og',
+    'deviceOrientation' => null,
+    'colorDepth' => '1bit',
+    'scaleLevel' => null,
+])
 
-<x-trmnl::screen>
-    <x-trmnl::mashup mashup-layout="{{ $mashupLayout }}">
-        {{-- The slot is used to pass the content of the mashup --}}
-        {!! $slot !!}
-    </x-trmnl::mashup>
-</x-trmnl::screen>
+@if(config('app.puppeteer_window_size_strategy') === 'v1')
+    <x-trmnl::screen colorDepth="{{$colorDepth}}">
+        <x-trmnl::mashup mashup-layout="{{ $mashupLayout }}">
+            {!! $slot !!}
+        </x-trmnl::mashup>
+    </x-trmnl::screen>
+@else
+    <x-trmnl::screen colorDepth="{{$colorDepth}}" no-bleed="{{$noBleed}}" dark-mode="{{$darkMode}}"
+                     device-variant="{{$deviceVariant}}" device-orientation="{{$deviceOrientation}}"
+                     scale-level="{{$scaleLevel}}">
+        <x-trmnl::mashup mashup-layout="{{ $mashupLayout }}">
+            {!! $slot !!}
+        </x-trmnl::mashup>
+    </x-trmnl::screen>
+@endif
