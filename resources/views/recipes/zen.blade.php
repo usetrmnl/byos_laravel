cb4663645280e04c60581b9f63eadef591da37cf
Benjamin Nussbaum <bnussbau@users.noreply.github.com>
2026-02-12 21:48:20 +0100

    fix: remove duplicate layout in zen recipe

--- resources/views/recipes/zen.blade.php
+++ resources/views/recipes/zen.blade.php
@@ -1,15 +1,13 @@
 {{--@dump($data)--}}
 @props(['size' => 'full'])
 <x-trmnl::view size="{{ $size }}">
-    <x-trmnl::layout>
-        <x-trmnl::layout class="layout--col">
-            <div class="b-h-gray-1">{{$data['data'][0]['a'] ?? ''}}</div>
-            @if (strlen($data['data'][0]['q'] ?? '') < 300 && $size != 'quadrant')
-                <p class="value">{{ $data['data'][0]['q'] ?? '' }}</p>
-            @else
-                <p class="value--small">{{ $data['data'][0]['q'] ?? '' }}</p>
-            @endif
-        </x-trmnl::layout>
+    <x-trmnl::layout class="layout--col">
+        <div class="b-h-gray-1">{{$data['data'][0]['a'] ?? ''}}</div>
+        @if (strlen($data['data'][0]['q'] ?? '') < 300 && $size != 'quadrant')
+            <p class="value">{{ $data['data'][0]['q'] ?? '' }}</p>
+        @else
+            <p class="value--small">{{ $data['data'][0]['q'] ?? '' }}</p>
+        @endif
     </x-trmnl::layout>
 
     <div class="title_bar">
